d0e0a1b8c237a469ef6a11c890bc9622f632065c
samrooney <sam_rooney@asl.org>
2014-10-10 10:51:28 +0100

    i changed it to hello world

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
-public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello, Github");
-  }
+import java.util.Scanner;
+
+public class Hello { 
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      System.out.println("enter a noun");
+      String noun1 = "";
+      noun1 = scnr.next(); 
+      System.out.println("Hi, i am a " + noun1);
+   }
+
 }
